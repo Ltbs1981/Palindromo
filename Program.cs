44ab09cccf6b5ca7c434afaacfd63c409c96a31c
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-08-03 18:28:45 -0300

    Adicionado do while, para poder testar mais vezes

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
-﻿Console.WriteLine("Digite uma palavra" );
-string palavra = Console.ReadLine();
-Console.WriteLine($"A palavra digitada foi: {palavra}.");
-
-//convertendo a palavra para um vetor
-
-char[] arr = palavra.ToCharArray();
-Array.Reverse(arr);
-
-string palindromo = new string(arr);
-
-if (palindromo == palavra)
+﻿do
 {
-    Console.WriteLine("essa palavra é um palíndromo!" );
-}else
-{
-    Console.WriteLine("palavra não é um palíndromo ");
-}
+    Console.WriteLine("Digite uma palavra");
+    string palavra = Console.ReadLine();
+    Console.WriteLine($"A palavra digitada foi: {palavra}.");
+
+    //convertendo a palavra para um vetor
+
+    char[] vetor = palavra.ToCharArray();
+    Array.Reverse(vetor);
+
+    string palindromo = new string(vetor);
+
+    if (palindromo == palavra)
+    {
+        Console.WriteLine("Essa palavra é um palíndromo!");
+    }
+    else
+    {
+        Console.WriteLine("A palavra não é um palíndromo ");
+    }
+    Console.WriteLine("Digite 1 para sair, ou qualquer outro número para testar outra palavra!");
+    int sair = int.Parse(Console.ReadLine());
+    if (sair == 1)
+    {
+        break;
+
+    }
+} while (true);
