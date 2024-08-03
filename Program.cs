ca42eba7d0e0b23531a5deefd0da0f8f14c65a8f
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-08-02 22:43:42 -0300

    forma procedural pronta e fodando

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿Console.WriteLine("Digite uma palavra" );
 string palavra = Console.ReadLine();
 Console.WriteLine($"A palavra digitada foi: {palavra}.");
+
+//convertendo a palavra para um vetor
+
+char[] arr = palavra.ToCharArray();
+Array.Reverse(arr);
+
+string palindromo = new string(arr);
+
+if (palindromo == palavra)
+{
+    Console.WriteLine("essa palavra é um palíndromo!" );
+}else
+{
+    Console.WriteLine("palavra não é um palíndromo ");
+}
